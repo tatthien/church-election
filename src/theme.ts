aef63d099d88230e7229fe4b61b9b32eb3d946a0
Thien Nguyen <tatthien.contact@gmail.com>
2023-10-06 07:29:23 +0700

    style: override modal title styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,14 @@
-import { createTheme } from "@mantine/core";
+import { createTheme, Modal } from "@mantine/core";
 
-export default createTheme({});
+export default createTheme({
+  components: {
+    Modal: Modal.extend({
+      styles: (theme) => ({
+        title: {
+          fontWeight: 600,
+          fontSize: theme.fontSizes.lg,
+        },
+      }),
+    }),
+  },
+});
